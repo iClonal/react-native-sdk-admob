ca5b8dcdb76c16bda3151dae5c5ba9a0f214c56c
Wei Jian <wei.jian.coder+github@gmail.com>
2019-01-14 17:24:24 +0800

    :bug: fix manifest metadata key

--- android/src/main/java/io/github/ichonal/sdkadmob/RNSGADCommon.java
+++ android/src/main/java/io/github/ichonal/sdkadmob/RNSGADCommon.java
@@ -7,7 +7,7 @@ public class RNSGADCommon {
     public static final String TEST_NATIVE_ADVANCED_ADUNIT_ID = "ca-app-pub-3940256099942544/2247696110";
 
     public static final String META_KEY_BANNER_ID = "io.github.ichonal.sdkadmob.GADBANNER_ID";
-    public static final String META_KEY_INTERSTITIAL_ID = "io.github.ichonal.sdkadmob.GADINTERSITIAL_ID";
+    public static final String META_KEY_INTERSTITIAL_ID = "io.github.ichonal.sdkadmob.GADINTERSTITIAL_ID";
     public static final String META_KEY_REWARDED_ID = "io.github.ichonal.sdkadmob.GADREWARDED_ID";
     public static final String META_KEY_NATIVE_ADVANCED_ID = "io.github.ichonal.sdkadmob.GADNATIVE_ID";
 }
